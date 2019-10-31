5cc0a36b95822b0ee53a55a685b27538d54b6d04
Prakhar Tripathi <prakhartripathi@Prakhars-MacBook-Pro.local>
2019-11-01 00:44:11 +0530

    updated the ui of music player

--- Song Studio/ViewControllers/MusicPlayerViewController.swift
+++ Song Studio/ViewControllers/MusicPlayerViewController.swift
@@ -7,17 +7,36 @@
 //
 
 import UIKit
+import SDWebImage
 
 class MusicPlayerViewController: UIViewController {
     
     var selectedSongIndex = 0
     var songList = [Song]()
 
+    @IBOutlet weak var navigationTitle: UINavigationItem!
+    @IBOutlet weak var coverImage: UIImageView!
+    @IBOutlet weak var playPauseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationTitle.title = songList[selectedSongIndex].name
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        coverImage.sd_setImage(with: URL(string: songList[selectedSongIndex].coverImageURL), completed: nil)
     }
     
     @IBAction func backButtonAction(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
+    }
+    @IBAction func prevButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func pauseButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func nextButtonAction(_ sender: Any) {
     }
 }

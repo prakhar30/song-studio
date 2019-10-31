b2502fdb707a25d23e3e87c80e8c1587e58e4986
Prakhar Tripathi <prakhartripathi@Prakhars-MacBook-Pro.local>
2019-11-01 00:57:50 +0530

    implemented play pause functionality

--- Song Studio/ViewControllers/MusicPlayerViewController.swift
+++ Song Studio/ViewControllers/MusicPlayerViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 import SDWebImage
+import AVFoundation
 
 class MusicPlayerViewController: UIViewController {
     
-    var selectedSongIndex = 0
+    var selectedSongIndex = 0 {
+        didSet {
+            print(selectedSongIndex)
+        }
+    }
     var songList = [Song]()
+    lazy var playerQueue : AVQueuePlayer = {
+        return AVQueuePlayer()
+    }()
 
     @IBOutlet weak var navigationTitle: UINavigationItem!
     @IBOutlet weak var coverImage: UIImageView!
@@ -26,6 +34,7 @@ class MusicPlayerViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         coverImage.sd_setImage(with: URL(string: songList[selectedSongIndex].coverImageURL), completed: nil)
+        playCurrentSong(atIndex: selectedSongIndex)
     }
     
     @IBAction func backButtonAction(_ sender: Any) {
@@ -35,8 +44,29 @@ class MusicPlayerViewController: UIViewController {
     }
     
     @IBAction func pauseButtonAction(_ sender: Any) {
+        if self.playerQueue.isPlaying {
+            self.playerQueue.pause()
+            self.playPauseButton.setTitle("Play", for: .normal)
+        } else {
+            self.playerQueue.play()
+            self.playPauseButton.setTitle("Pause", for: .normal)
+        }
     }
     
     @IBAction func nextButtonAction(_ sender: Any) {
+    }
+    
+    func playCurrentSong(atIndex: Int) {
+        if let url = URL(string: songList[atIndex].url) {
+            let playerItem = AVPlayerItem.init(url: url)
+            self.playerQueue.insert(playerItem, after: nil)
+            self.playerQueue.play()
+        }
+    }
+}
+
+extension AVPlayer {
+    var isPlaying: Bool {
+        return rate != 0 && error == nil
     }
 }

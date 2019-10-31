e2c3d8eaafe30a0191c5a528e34f40cf8d16a29d
Prakhar Tripathi <prakhartripathi@Prakhars-MacBook-Pro.local>
2019-10-31 22:58:32 +0530

    first commit

---
+++ Song Studio/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Song Studio
+//
+//  Created by Prakhar Tripathi on 31/10/19.
+//  Copyright © 2019 Prakhar Tripathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
